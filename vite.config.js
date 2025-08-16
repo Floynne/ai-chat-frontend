8b75340e2a2210da181a4bef633aaf26e33be95d
Floynne <floynnee@gmail.com>
2025-08-17 04:54:05 +0500

    build: implement ultimate fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Добавляем эту секцию, чтобы явно указать, как работать с React
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
 })
