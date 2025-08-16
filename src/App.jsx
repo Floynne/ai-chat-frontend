8e68c802c94990a46a21db49bf4889854fb3bb50
Floynne <floynnee@gmail.com>
2025-08-17 04:48:05 +0500

    Initial frontend commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { motion, AnimatePresence } from 'framer-motion';
 import './App.css';
 
-const BACKEND_URL = 'http://localhost:3000';
+const BACKEND_URL = 'https://ai-chat-server-k50v.onrender.com';
 
 // --- КОМПОНЕНТЫ ---
 const SendIcon = () => ( <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg> );
